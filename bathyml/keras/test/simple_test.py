262b67cb9717c56148804fe767d73f435a7f0603
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-07-26 22:17:32 -0400

    Update simple_test

--- bathyml/keras/test/simple_test.py
+++ bathyml/keras/test/simple_test.py
@@ -1,5 +1,6 @@
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.optimizers import SGD
 import os, numpy as np
 import matplotlib.pyplot as plt
 from keras.callbacks import TensorBoard
@@ -11,28 +12,56 @@ DATA = os.path.join( os.path.dirname( os.path.dirname( os.path.dirname(HERE) ) )
 ddir = os.path.join(DATA, "csv")
 tb_log_dir=f"{ddir}/logs/tb"
 nBands = 21
-nEpochs = 50
+nEpochs = 500
+learningRate=0.01
+momentum=0.9
+shuffle=False
+validation_fraction=0.2
+decay=0.
+nRuns = 10
+nesterov=True
+initWtsMethod="glorot_uniform"   # lecun_uniform glorot_normal glorot_uniform he_normal lecun_normal he_uniform
+activation='relu' # 'tanh'
 print( f"TensorBoard log dir: {tb_log_dir}")
 
-x_training_data = os.path.join(ddir, "temp_X_train.csv")
-y_training_data = os.path.join(ddir, "temp_Y_train.csv")
-x_training_data_array = np.loadtxt(x_training_data, delimiter=',')[:,:nBands]
-y_training_data_array = np.loadtxt(y_training_data, delimiter=',')
-x_train = x_training_data_array/x_training_data_array.max(axis=0)
-y_train = y_training_data_array/y_training_data_array.max(axis=0)
+x_training_data: str = os.path.join(ddir, "temp_X_train.csv")
+y_training_data: str = os.path.join(ddir, "temp_Y_train.csv")
+x_training_data_array: np.ndarray = np.loadtxt(x_training_data, delimiter=',')[:,:nBands]
+y_training_data_array: np.ndarray = np.loadtxt(y_training_data, delimiter=',')
+x_train: np.ndarray = x_training_data_array/x_training_data_array.max(axis=0)
+y_train: np.ndarray = y_training_data_array/y_training_data_array.max(axis=0)
 
-model = Sequential()
 tensorboard = TensorBoard( log_dir=f"{tb_log_dir}/{time()}" )
+sgd = SGD( learningRate, momentum, decay, nesterov )
+index_permutation = np.random.permutation( x_train.shape[0] )
+ens_min_val_loss = 10000
+best_model_index = None
+models = {}
 
-model.add( Dense( units=64, activation='relu', input_dim=nBands ) )
-model.add( Dense( units=1 ) )
+for model_index in range( nRuns ):
 
-model.compile(loss='mse', optimizer='sgd', metrics=['accuracy'])
+    model = Sequential()
+    model.add( Dense( units=32, activation=activation, input_dim=nBands, kernel_initializer = initWtsMethod ) )
+    model.add( Dense( units=16, activation=activation, kernel_initializer = initWtsMethod ) )
+    model.add( Dense( units=1, kernel_initializer = initWtsMethod ) )
+    model.compile(loss='mse', optimizer=sgd, metrics=['accuracy'])
 
-model.fit( x_train, y_train, epochs=nEpochs, callbacks=[tensorboard] )
+    x_train_resorted = x_train[ index_permutation ]
+    y_train_resorted = y_train[ index_permutation ]
 
-prediction = model.predict( x_train )
+    history = model.fit( x_train_resorted, y_train_resorted, epochs=nEpochs, validation_split=validation_fraction, verbose=0, shuffle=shuffle, callbacks=[tensorboard] )
 
+    val_loss = np.array( history.history['val_loss'] )
+    final_val_loss = val_loss[-1]
+    min_val_loss = val_loss.min( axis=0, initial=10000 )
+    models[ model_index ] = ( model, final_val_loss, min_val_loss )
+    if min_val_loss < ens_min_val_loss:
+        ens_min_val_loss = min_val_loss
+        best_model_index = model_index
+
+( best_model, final_val_loss, min_val_loss ) = models[best_model_index]
+print( f"best_model_index = {best_model_index}, final_val_loss={final_val_loss}, min_val_loss={min_val_loss}")
+prediction = best_model.predict( x_train )
 plt.plot(range(y_train.shape[0]), y_train, "b--", label="training data")
 plt.plot(range(prediction.shape[0]), prediction, "r--", label="prediction")
 plt.show()
