b7a96d5eb35f67b3465ea2fc0676ed3751edb666
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-11-25 17:04:31 -0500

    Add apply estimators

--- framework/estimator/test/full_dataset-training.py
+++ framework/estimator/test/full_dataset-training.py
@@ -14,7 +14,7 @@ show_plots = False
 modelTypes = [ "mlp", "rf", "svr", "nnr" ]
 
 parameters = dict(
-    mlp=dict( max_iter=150, learning_rate="constant", solver="adam", early_stopping=False ),
+    mlp=dict( max_iter=500, learning_rate="constant", solver="adam", early_stopping=False ),
     rf=dict(n_estimators=70, max_depth=20),
     svr=dict(C=5.0, gamma=0.5),
     nnr=dict( n_neighbors=5, weights='distance' ),
